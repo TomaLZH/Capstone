f17ba72d2b7c0d4d62295446364793225c7d5545
TomaLZH <mynameiszhenghan@gmail.com>
2025-02-04 15:24:03 +0800

    Add chat log update functionality and persist chat history in the database

--- databasefunctions.py
+++ databasefunctions.py
@@ -1,5 +1,6 @@
 import sqlite3
 import hashlib
+import pickle
 
 # Function to create a new database connection
 def connect_db():
@@ -38,3 +39,25 @@ def authenticate_user(username, password):
         conn.close()
 
         return {"status": 201, "message": "User registered successfully!", "user": new_user}
+
+
+# Function to update row with chat log
+def update_chat_log(username, chat_data):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE users SET data = ? WHERE username = ?", (chat_data, username))
+    conn.commit()
+    conn.close()
+
+def get_chat_log(username):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT data FROM users WHERE username = ?", (username,))
+    chat_data = cursor.fetchone()
+    conn.close()
+    if chat_data:
+        return pickle.loads(chat_data[0])
+    else:
+        return None
